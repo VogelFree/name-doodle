65d3a3b37de98bbfc30201f23e385041f4bf510f
Daniel Vogel <kontakt@danielvogel.net>
2019-04-09 21:15:24 +0200

    Add validation methods for publisher info and name info

--- src/Collect.Domain/SaveNames.Implementation.fs
+++ src/Collect.Domain/SaveNames.Implementation.fs
@@ -6,5 +6,19 @@ module internal NameDoodle.Domain.Collect.SaveNames.Implementation
 type CheckNameInfoExists =
   UnvalidatedNameInfo -> bool
 
+type ValidatedNameInfo = UnvalidatedNameInfo
+
+type ValidateNameInfo =
+  CheckNameInfoExists
+    -> UnvalidatedNameInfo
+    -> Result<ValidatedNameInfo, NameAlreadyExistsError>
+
 type CheckPublisherInfoExists =
   UnvalidatedPublisherInfo -> bool
+
+type ValidatedPublisherInfo = UnvalidatedPublisherInfo
+
+type ValidatePublisherInfo =
+  CheckPublisherInfoExists
+    -> UnvalidatedPublisherInfo
+    -> Result<ValidatedPublisherInfo, AlreadySendNamesError>
